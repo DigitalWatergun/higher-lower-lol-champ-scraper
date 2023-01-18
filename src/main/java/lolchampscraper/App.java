fbc7bd2611dfeb9c91583f9262a5c96db7e84310
Philip <philipwinduong@gmail.com>
2023-01-17 21:42:38 -0800

    Add HtmlUnit and WRScraper

--- src/main/java/lolchampscraper/App.java
+++ src/main/java/lolchampscraper/App.java
@@ -9,7 +9,11 @@ public class App {
         ArrayList<String> championNames = champions.getChampionNames();
 
         System.out.println(championNames);
-        System.out.println(championNames.getClass().getSimpleName());
         System.out.println(championNames.size());
+
+        WRScraper wrScraper = new WRScraper();
+        for (String championName : championNames) {
+            wrScraper.scrapeUgg(championName);
+        }
     }
 }
---
+++ src/main/java/lolchampscraper/WRScraper.java
@@ -0,0 +1,25 @@
+package lolchampscraper;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlDivision;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class WRScraper {
+    public void scrapeUgg(String championName) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+        try (WebClient client = new WebClient()) {
+            client.getOptions().setCssEnabled(false);
+            client.getOptions().setJavaScriptEnabled(false);
+
+            String baseUrl = "https://u.gg/lol/champions/%s/build";
+            String searchUrl = String.format(baseUrl, championName);
+
+            HtmlPage page = (HtmlPage) client.getPage(searchUrl);            
+            HtmlDivision winRate = (HtmlDivision) page.getByXPath("//div[contains(@class, 'champion-ranking-stats-normal')]/div[contains(@class, 'win-rate')]/div[contains(@class, 'value')]").get(0);
+            System.out.println(championName + " " + winRate.asNormalizedText());
+        }
+    }
+}
