e14dcbf090a774b708e56513cc4bd74625c9a391
Philip <philipwinduong@gmail.com>
2023-01-21 14:27:14 -0800

    Refactor championInfo map entry to clean up datastore.java

--- src/main/java/higherlowerlolchamp/scraper/App.java
+++ src/main/java/higherlowerlolchamp/scraper/App.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException, SQLException {
@@ -13,16 +14,22 @@ public class App {
         System.out.println("Number of champion names pulled: " + championNames.size());
 
         System.out.println("Pulling matches played for each champion...");
-        HashMap<String, String> champMatchesPlayed = new HashMap<String, String>();
+        Map<String, Map<String, String>> championData = new HashMap<>();
         for (String championName : championNames) {
             String matchesPlayed = Scraper.scrapeUgg(championName);
-            champMatchesPlayed.put(championName, matchesPlayed);
-            System.out.println(championName + " " + matchesPlayed);
+
+            Map<String, String> championInfo = new HashMap<>();
+            championInfo.put("championName", championName);
+            championInfo.put("matchesPlayed", matchesPlayed);
+            championInfo.put("loadingScreenUrl", String
+                    .format("http://ddragon.leagueoflegends.com/cdn/img/champion/loading/%s_0.jpg", championName));
+            championData.put(championName, championInfo);
+            System.out.println(championName + " " + championInfo);
         }
 
-        System.out.println("Upserting champion matches played to database...");
+        System.out.println("Upserting champion data to database...");
         Connection connection = Datastore.connectToPostgres();
-        Datastore.batchInsert(connection, champMatchesPlayed);
+        Datastore.batchInsert(connection, championData);
         Datastore.closePostgres(connection);
         System.out.println("Done!!");
     }
--- src/main/java/higherlowerlolchamp/scraper/Datastore.java
+++ src/main/java/higherlowerlolchamp/scraper/Datastore.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
-import java.util.HashMap;
 import java.util.Map;
 
 import io.github.cdimascio.dotenv.Dotenv;
@@ -24,17 +23,19 @@ public class Datastore {
         connection.close();
     }
 
-    public static void batchInsert(Connection connection, HashMap<String, String> champMatchesPlayed) throws SQLException {
+    public static void batchInsert(Connection connection, Map<String, Map<String, String>> championData) throws SQLException {
         String sql = "INSERT into champions (champion_name, matches_played, loading_screen_url) VALUES (?, ?, ?) " +
                 "ON CONFLICT (champion_name) DO UPDATE SET matches_played = excluded.matches_played, loading_screen_url = excluded.loading_screen_url " +
                 "WHERE champions.matches_played <> excluded.matches_played OR champions.loading_screen_url <> excluded.loading_screen_url";
         PreparedStatement sqlStatement = connection.prepareStatement(sql);
 
-        for (Map.Entry<String, String> entry : champMatchesPlayed.entrySet()) {
-            String champion = entry.getKey();
-            String matchesPlayed = entry.getValue();
-            String loadingScreenUrl = String.format("http://ddragon.leagueoflegends.com/cdn/img/champion/loading/%s_0.jpg", champion);
-            sqlStatement.setString(1, champion);
+        for (Map.Entry<String, Map<String, String>> championDataEntry : championData.entrySet()) {
+            Map<String, String> championInfo = championDataEntry.getValue();
+            String championName = championInfo.get("championName");
+            String matchesPlayed = championInfo.get("matchesPlayed");
+            String loadingScreenUrl = championInfo.get("loadingScreenUrl");
+
+            sqlStatement.setString(1, championName);
             sqlStatement.setString(2, matchesPlayed);
             sqlStatement.setString(3, loadingScreenUrl);
             sqlStatement.addBatch();
