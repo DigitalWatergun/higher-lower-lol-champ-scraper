2d73bfc7b624476660c03722614c9fa86be2049e
Philip <philipwinduong@gmail.com>
2023-01-21 13:08:08 -0800

    Add loading_screen_url dynamic generation

--- src/main/java/higherlowerlolchamp/scraper/Datastore.java
+++ src/main/java/higherlowerlolchamp/scraper/Datastore.java
@@ -25,16 +25,18 @@ public class Datastore {
     }
 
     public static void batchInsert(Connection connection, HashMap<String, String> champMatchesPlayed) throws SQLException {
-        String sql = "INSERT into champions (champion_name, matches_played) VALUES (?, ?) " +
-                "ON CONFLICT (champion_name) DO UPDATE SET matches_played = excluded.matches_played " +
-                "WHERE champions.matches_played <> excluded.matches_played";
+        String sql = "INSERT into champions (champion_name, matches_played, loading_screen_url) VALUES (?, ?, ?) " +
+                "ON CONFLICT (champion_name) DO UPDATE SET matches_played = excluded.matches_played, loading_screen_url = excluded.loading_screen_url " +
+                "WHERE champions.matches_played <> excluded.matches_played OR champions.loading_screen_url <> excluded.loading_screen_url";
         PreparedStatement sqlStatement = connection.prepareStatement(sql);
 
         for (Map.Entry<String, String> entry : champMatchesPlayed.entrySet()) {
             String champion = entry.getKey();
             String matchesPlayed = entry.getValue();
+            String loadingScreenUrl = String.format("http://ddragon.leagueoflegends.com/cdn/img/champion/loading/%s_0.jpg", champion);
             sqlStatement.setString(1, champion);
             sqlStatement.setString(2, matchesPlayed);
+            sqlStatement.setString(3, loadingScreenUrl);
             sqlStatement.addBatch();
         }
 
